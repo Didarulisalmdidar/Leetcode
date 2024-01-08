f18ccbec65c17062ca35061082df5c3f63583fe9
Didarul Islam Didar <93069023+Didarulisalmdidar@users.noreply.github.com>
2024-01-08 22:28:11 +0600

    Time: 18 ms (22.34%), Space: 15.4 MB (16.47%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,7 +1,35 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        priority_queue<pair<int,int>>pq;
+        map<int,int>mp;
+        for(auto it:nums)
+            mp[it]++;
+        int n=nums.size();
+        vector<int>v[n+1];
+        for(auto it:mp)
+            v[it.second].push_back(it.first);
+        vector<int>ans;
+        for(int i=n;i>0;i--)
+        {
+            if(v[i].size()!=0)
+            {
+                for(auto it:v[i])
+                {
+                    ans.push_back(it);
+                    k--;
+                    if(k==0)
+                        break;
+                }
+            }
+            if(k==0)
+                break;
+        }
+        return ans;
+        
+        
+// tc= O(N)        
+            
+        /*priority_queue<pair<int,int>>pq;
         map<int,int>mp;
         for(auto it:nums)
         {
@@ -23,8 +51,9 @@ public:
          
          
      }
-        return ans;
+        return ans;*/
     }
 };
-
+// 1st approach in comment by max heap
 //TC= O(NLOGN) NOT efficient
+//haepify take N times and eash push in priority_queue takes logn time . Can it solve in Linear time ? Yeah -> Bucket Sort 
