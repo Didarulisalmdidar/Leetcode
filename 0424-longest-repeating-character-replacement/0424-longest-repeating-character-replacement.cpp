c24cc447eebad8040796a989300638395c267ab1
Didarul Islam Didar <93069023+Didarulisalmdidar@users.noreply.github.com>
2024-01-13 17:12:54 +0600

    Time: 93 ms (6.63%), Space: 7.4 MB (36.23%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+       map<char,int>mp;
+       int l=0,r=0;
+        int ans=0;
+       while(r!=s.size())
+       {
+           mp[s[r]]++;
+           int mx=0;
+           for(char ch='A';ch<='Z';ch++)
+           {
+               mx=max(mx,mp[ch]);
+           }
+           int w_size=r-l+1;
+           if(w_size-mx<=k)
+           {
+               ans=max(ans,w_size);
+               r++;
+           }
+           else
+           {
+               mp[s[l]]--;
+               l++;
+               w_size=r-l+1;
+               ans=max(ans,w_size);
+               r++;
+           }
+       }
+        return ans;
+    }
+};
